7ecdf0d3895c9d278089e4d6764d19e0d29a55f1
hamzioui <rikilincoln@gmail.com>
2018-11-13 10:48:58 +0100

    config

---
+++ public/js/express.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    var params = {test:'test'};
+    var link ='test';
+    var myInit = { method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+    };
+    fetch(link,myInit)
+        .then(res => res.json())
+        .then(response => console.log('Success:', response))
+        .catch(error => console.error('Error:', error));
+});
